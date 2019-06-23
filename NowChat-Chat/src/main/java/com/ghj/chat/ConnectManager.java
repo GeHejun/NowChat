e78c700c5cb937fda9da00bd3363e9b2b258c52d
wanghui <1515764144@qq.com>
2019-06-24 00:54:12 +0800

    more

--- NowChat-Chat/src/main/java/com/ghj/chat/ConnectManager.java
+++ NowChat-Chat/src/main/java/com/ghj/chat/ConnectManager.java
@@ -1,5 +1,18 @@
 package com.ghj.chat;
 
+import io.netty.channel.Channel;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ConnectManager {
 
+    private static ConcurrentHashMap CHANNEL_MAP = new ConcurrentHashMap(16);
+
+    public static void putChannel(String requestId, Channel channel) {
+        CHANNEL_MAP.put(requestId, channel);
+    }
+
+    public static Channel getChannel(String requestId) {
+        return (Channel) CHANNEL_MAP.get(requestId);
+    }
 }
--- NowChat-Chat/src/main/java/com/ghj/chat/Session.java
+++ NowChat-Chat/src/main/java/com/ghj/chat/Session.java
@@ -2,4 +2,5 @@ package com.ghj.chat;
 
 public class Session {
 
+    String sessionId;
 }
