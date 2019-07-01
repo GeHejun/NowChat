20823473befdfd61d34d440e92baea8539a46a8d
gehj <gehj@ziroom.com>
2019-07-01 13:55:12 +0800

    服务端

--- NowChat-Registry/src/main/java/com/ghj/registry/Registry.java
+++ NowChat-Registry/src/main/java/com/ghj/registry/Registry.java
@@ -7,7 +7,14 @@ import java.util.concurrent.ConcurrentHashMap;
  * @date 2019/7/112:49
  */
 public class Registry {
-    public static ConcurrentHashMap serverMap = new ConcurrentHashMap(32);
 
+    public static ConcurrentHashMap<Long, ServerSession> SERVER_SESSION_MAP = new ConcurrentHashMap(32);
 
+    public static void putServerSession(Long machineSerialNumber, ServerSession serverSession) {
+        SERVER_SESSION_MAP.put(machineSerialNumber, serverSession);
+    }
+
+    public static ServerSession getServerSession(Long machineSerialNumber) {
+        return SERVER_SESSION_MAP.get(machineSerialNumber);
+    }
 }
