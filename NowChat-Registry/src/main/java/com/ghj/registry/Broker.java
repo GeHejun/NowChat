46167cb60b7d9ec1a64e1d41369bba49a06a67be
gehj <gehj@ziroom.com>
2019-07-01 12:50:11 +0800

    服务端

--- NowChat-Registry/src/main/java/com/ghj/registry/Broker.java
+++ NowChat-Registry/src/main/java/com/ghj/registry/Broker.java
@@ -4,6 +4,6 @@ package com.ghj.registry;
  * @author GeHejun
  * @date 2019/7/1 11:22
  */
-public class DistributionCentre {
+public class Broker {
 
 }
---
+++ NowChat-Registry/src/main/java/com/ghj/registry/Registry.java
@@ -0,0 +1,8 @@
+package com.ghj.registry;
+
+/**
+ * @author gehj
+ * @date 2019/7/112:49
+ */
+public class Registry {
+}
