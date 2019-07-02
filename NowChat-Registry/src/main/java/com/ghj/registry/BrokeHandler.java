16091a27afdcf5b7bb70ee438170f806d60de508
wanghui <1515764144@qq.com>
2019-07-02 23:16:34 +0800

    服务端

--- NowChat-Registry/src/main/java/com/ghj/registry/BrokeHandler.java
+++ NowChat-Registry/src/main/java/com/ghj/registry/BrokeHandler.java
@@ -3,8 +3,11 @@ package com.ghj.registry;
 
 import com.ghj.protocol.RegisterMessageProto.RegisterMessage;
 import com.ghj.protocol.RequestMessageProto.RequestMessage;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.Objects;
 
 /**
  * @author gehj
@@ -14,10 +17,19 @@ public class BrokeHandler extends SimpleChannelInboundHandler {
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) {
         if (o instanceof RegisterMessage) {
-
+            RegisterMessage registerMessage = (RegisterMessage) o;
+            ServerSession serverSession = ServerSession.builder().machineSerialNumber(registerMessage.getMachineSerialNumber())
+                    .ip(registerMessage.getIp()).port(registerMessage.getPort())
+                    .channel(channelHandlerContext.channel()).build();
+            Registry.putServerSession(registerMessage.getMachineSerialNumber(), serverSession);
         }
         if (o instanceof RequestMessage) {
-
+            ServerSession serverSession = Registry.getServerSession(((RequestMessage) o).getMachineSerialNumber());
+            Channel serverChannel = serverSession.getChannel();
+//            if (Objects.isNull(serverChannel)) {
+//
+//            }
+            serverChannel.writeAndFlush(o);
         }
     }
 }
--- NowChat-Registry/src/main/java/com/ghj/registry/ServerSession.java
+++ NowChat-Registry/src/main/java/com/ghj/registry/ServerSession.java
@@ -1,12 +1,20 @@
 package com.ghj.registry;
 
+import io.netty.channel.Channel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 /**
  * @author gehj
  * @date 2019/7/1 13:32
  */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ServerSession {
 
     private String ip;
@@ -14,5 +22,7 @@ public class ServerSession {
     private int port;
 
     private Long machineSerialNumber;
+
+    private Channel channel;
 
 }
