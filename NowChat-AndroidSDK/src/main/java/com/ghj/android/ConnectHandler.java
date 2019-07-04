3195d84e0b3c62c28515c5260ccabed4e89d1146
gehj <gehj@ziroom.com>
2019-07-04 15:07:59 +0800

    服务端

---
+++ NowChat-AndroidSDK/src/main/java/com/ghj/android/ConnectHandler.java
@@ -0,0 +1,15 @@
+package com.ghj.android;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @author gehj
+ * @date 2019/7/415:03
+ */
+public class ConnectHandler extends SimpleChannelInboundHandler {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+}
---
+++ NowChat-AndroidSDK/src/main/java/com/ghj/android/Connnector.java
@@ -0,0 +1,42 @@
+package com.ghj.android;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author gehj
+ * @date 2019/7/414:52
+ */
+public class Connnector {
+
+    public void connect(String host, int port) {
+        NioEventLoopGroup nioEventLoopGroup = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(nioEventLoopGroup)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>()
+                    {
+                        @Override
+                        public void initChannel(SocketChannel ch)
+                        {
+                            ch.pipeline().addLast(new ConnectHandler());
+                        }
+                    });
+            // 发起异步连接操作
+            ChannelFuture f = bootstrap.connect(host, port).sync();
+
+            // 等待客户端链路关闭
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            nioEventLoopGroup.shutdownGracefully();
+        }
+
+    }
+}
