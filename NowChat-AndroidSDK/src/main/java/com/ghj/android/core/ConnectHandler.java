ffaa1272ef1dc5fe2e89db4377bc0196329718ff
gehj <gehj@ziroom.com>
2019-07-05 17:45:48 +0800

    服务端

--- NowChat-AndroidSDK/src/main/java/com/ghj/android/core/ConnectHandler.java
+++ NowChat-AndroidSDK/src/main/java/com/ghj/android/core/ConnectHandler.java
@@ -1,5 +1,6 @@
 package com.ghj.android.core;
 
+import com.ghj.common.base.Constant;
 import com.ghj.protocol.AckMessageProto;
 import com.ghj.protocol.RequestMessageProto;
 import io.netty.channel.ChannelHandlerContext;
@@ -16,7 +17,9 @@ public class ConnectHandler extends SimpleChannelInboundHandler {
     protected void channelRead0(ChannelHandlerContext ctx, Object msg)  {
         if (msg instanceof AckMessageProto.AckMessage) {
             AckMessageProto.AckMessage ackMessage = (AckMessageProto.AckMessage) msg;
-
+            if (Constant.LOGIN_SUCCESS_CODE == ackMessage.getCode()) {
+                MessageManager.getInstance().takeMessage();
+            }
         }
         if (msg instanceof RequestMessageProto.RequestMessage) {
             RequestMessageProto.RequestMessage requestMessage = (RequestMessageProto.RequestMessage) msg;
--- NowChat-AndroidSDK/src/main/java/com/ghj/android/core/MessageManager.java
+++ NowChat-AndroidSDK/src/main/java/com/ghj/android/core/MessageManager.java
@@ -24,7 +24,6 @@ public class MessageManager {
         if (messageManager == null) {
             synchronized (MessageManager.class) {
                 messageManager = new MessageManager();
-                messageManager.takeMessage();
             }
         }
         return messageManager;
