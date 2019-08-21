228e5628d22b43324b8d4c476b5f81861df82f4a
gehj <gehj@ziroom.com>
2019-08-21 18:20:10 +0800

    web

--- NowChat-Web/src/main/java/com/ghj/web/controller/TestController.java
+++ NowChat-Web/src/main/java/com/ghj/web/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.ghj.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author gehj
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TestController {
 
+
+
+    @RequestMapping("/test")
     public void test() {
 
     }
