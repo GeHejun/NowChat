1686619872201142c92facc1b7da020581677c09
gehj <gehj@ziroom.com>
2019-08-21 18:04:12 +0800

    web

---
+++ NowChat-Web/src/main/java/com/ghj/web/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.ghj.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author gehj
+ * @version 1.0
+ * @description TODO
+ * @date 2019/8/21 18:00
+ */
+@Controller
+public class TestController {
+
+    public void test() {
+
+    }
+}
