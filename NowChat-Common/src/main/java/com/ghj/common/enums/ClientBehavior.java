a9afc85bd0255baf0427c9b674c61de2c20669f0
wanghui <1515764144@qq.com>
2019-08-22 22:11:44 +0800

    Merge branch 'master' of https://github.com/GeHejun/NowChat
    # Conflicts:
    #	NowChat-Client/NowChat-Client.iml

--- NowChat-Common/src/main/java/com/ghj/common/enums/ClientBehavior.java
+++ NowChat-Common/src/main/java/com/ghj/common/enums/ClientBehavior.java
@@ -3,6 +3,6 @@ package com.ghj.common.enums;
  * @author GeHejun
  * @date 2019-06-24
  */
-public enum MessageBehavior {
+public enum ClientBehavior {
     LOGIN,PING,MESSAGE,LOGIN_OUT,REGISTER,REGISTRY_ACK,NOTIFY
 }
