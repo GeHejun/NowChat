31db65946f9c344378dc93a28777377e54ed2b06
wanghui <1515764144@qq.com>
2019-08-27 02:32:49 +0800

    登录集成

--- NowChat-Web/src/main/resources/static/js/ws.js
+++ NowChat-Web/src/main/resources/static/js/ws.js
@@ -191,17 +191,19 @@ function onMessage(evt) {
                     });
 
             } else {
-                layim.getMessage({
-                    username: buffer.name //消息来源用户名
-                    ,avatar: buffer.headPortrait //消息来源用户头像
-                    ,id: buffer.toUserId //消息的来源ID（如果是私聊，则是用户id，如果是群聊，则是群组id）
-                    ,type: buffer.messageDirect == 1 ? "friend" :"group" //聊天窗口来源类型，从发送消息传递的to里面获取
-                    ,content: buffer.connect //消息内容
-                    ,cid: buffer.id //消息id，可不传。除非你要对消息进行一些操作（如撤回）
-                    ,mine: false //是否我发送的消息，如果为true，则会显示在右方
-                    ,fromid: buffer.fromUserId //消息的发送者id（比如群组中的某个消息发送者），可用于自动解决浏览器多窗口时的一些问题
-                    ,timestamp: new Date() //服务端时间戳毫秒数。注意：如果你返回的是标准的 unix 时间戳，记得要 *1000
-                });
+                if (buffer.messageBehavior != 5)  {
+                    layim.getMessage({
+                        username: buffer.name //消息来源用户名
+                        ,avatar: buffer.headPortrait //消息来源用户头像
+                        ,id: buffer.toUserId //消息的来源ID（如果是私聊，则是用户id，如果是群聊，则是群组id）
+                        ,type: buffer.messageDirect == 1 ? "friend" :"group" //聊天窗口来源类型，从发送消息传递的to里面获取
+                        ,content: buffer.connect //消息内容
+                        ,cid: buffer.id //消息id，可不传。除非你要对消息进行一些操作（如撤回）
+                        ,mine: false //是否我发送的消息，如果为true，则会显示在右方
+                        ,fromid: buffer.fromUserId //消息的发送者id（比如群组中的某个消息发送者），可用于自动解决浏览器多窗口时的一些问题
+                        ,timestamp: new Date() //服务端时间戳毫秒数。注意：如果你返回的是标准的 unix 时间戳，记得要 *1000
+                    });
+                }
             }
         }
     });
