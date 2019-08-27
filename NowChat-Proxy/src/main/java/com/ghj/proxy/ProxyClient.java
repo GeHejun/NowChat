d3f8c2933911266c8f65a2c70871ed3380ef3eff
gehj <gehj@ziroom.com>
2019-08-27 11:47:00 +0800

    中间代理层

---
+++ NowChat-Proxy/src/main/java/com/ghj/proxy/ProxyClient.java
@@ -0,0 +1,10 @@
+package com.ghj.proxy;
+
+/**
+ * @author gehj
+ * @version 1.0
+ * @description TODO
+ * @date 2019/8/27 11:46
+ */
+public class ProxyClient {
+}
---
+++ NowChat-Proxy/src/main/java/com/ghj/proxy/ProxyStarter.java
@@ -0,0 +1,13 @@
+package com.ghj.proxy;
+
+/**
+ * @author gehj
+ * @version 1.0
+ * @description TODO
+ * @date 2019/8/27 11:45
+ */
+public class ProxyStarter {
+    public static void main(String[] args) {
+
+    }
+}
