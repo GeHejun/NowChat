58acd7e4f7c1fd81fa3262c01aef5761cf881a3f
gehj <gehj@ziroom.com>
2019-09-12 15:09:49 +0800

    创建群聊

--- NowChat-Common/src/main/java/com/ghj/common/netty/Register.java
+++ NowChat-Common/src/main/java/com/ghj/common/netty/Register.java
@@ -57,6 +57,7 @@ public class Register {
                     connector.stop();
                     throw new ServerException();
                 }
+
                 //获取本地ip地址
                 InetSocketAddress inetSocketAddress = (InetSocketAddress) channelFuture.channel().localAddress();
                 String localNetworkIp = inetSocketAddress.getAddress().getHostAddress();
